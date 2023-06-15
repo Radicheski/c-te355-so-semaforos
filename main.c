e36b20035d9e1c0856626ef299352c1351d3d095
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-06-15 19:43:07 -0300

    Adicionado método mostraVagas

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+
 int vagas[10][10];
 
+void mostraVagas() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf("%s", vagas[i][j] == 0 ? " " : "X");
+    }
+    printf("\n");
+  }
+}
+
 int main(void) {
+  mostraVagas();
   return 0;
 }
+
