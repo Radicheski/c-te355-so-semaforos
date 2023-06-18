5ada0eb67e99b464864d648ff36f5d272ade91c3
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-06-18 16:17:47 -0300

    Adicionado controle de vaga livre

--- main.c
+++ main.c
@@ -28,11 +28,69 @@ const Estado TRANQUILO = {2, 1};
 const Estado OCIOSO = {4, 1};
 const Estado CRITICO = {1, 5};
 
-int main(void) {
-  srand(time(NULL));
-  for (int i = 0; i < 5; i++) {
-    printf("%d\n", tempoEspera(5));
+typedef struct vaga {
+  int numero;
+  struct vaga *proxima;
+} Vaga;
+
+Vaga *proximaVaga = NULL;
+
+void criaVagas() {
+  for (int i = 99; i >= 0; i--) {
+    Vaga *v = (Vaga *) malloc(sizeof(Vaga));
+    v->numero = i;
+    v->proxima = proximaVaga;
+    proximaVaga = v;
   }
+}
+
+Vaga* ocupaVaga() {
+  if (proximaVaga == NULL) {
+    return NULL;
+  }
+
+  Vaga *vaga = proximaVaga;
+  proximaVaga = vaga->proxima;
+  vaga->proxima = NULL;
+
+  int i = vaga->numero / 10;
+  int j = vaga->numero % 10;
+  vagas[i][j] = 1;
+
+  return vaga;
+}
+
+void liberaVaga(Vaga *vaga) {
+  vaga->proxima = proximaVaga;
+  proximaVaga = vaga;
+
+  int i = vaga->numero / 10;
+  int j = vaga->numero % 10;
+  vagas[i][j] = 0;
+}
+
+void destroiVagas() {
+  Vaga *vaga = proximaVaga;
+  while (vaga != NULL) {
+    Vaga *temp = vaga;
+    vaga = vaga->proxima;
+    free(temp);
+  }
+}
+
+int main(void) {
+  criaVagas();
+
+  Vaga *v = NULL;
+
+  do {
+    v = ocupaVaga();
+    printf("%d\n", v->numero);
+    liberaVaga(v); // Causa loop infinito
+  } while (v != NULL);
+
+  destroiVagas();
+
   return 0;
 }
 
