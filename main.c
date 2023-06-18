c3aa84975adb50ed49b6c72969e9244ed898a068
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-06-18 15:46:28 -0300

    Adicionado estados

--- main.c
+++ main.c
@@ -17,6 +17,17 @@ int tempoEspera(int max) {
   return rand() % max;
 }
 
+typedef struct {
+  int entrada;
+  int saida;
+} Estado;
+
+const Estado BALANCEADO_LENTO = {2, 2};
+const Estado BALANCEADO_RAPIDO = {1, 1};
+const Estado TRANQUILO = {2, 1};
+const Estado OCIOSO = {4, 1};
+const Estado CRITICO = {1, 5};
+
 int main(void) {
   srand(time(NULL));
   for (int i = 0; i < 5; i++) {
